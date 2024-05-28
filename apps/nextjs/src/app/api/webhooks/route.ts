e0b39c88541da46c9fbb11f7d85aeed9562e4aba
dobzacode <contact@corentinkittel.fr>
2024-05-28 17:16:47 +0200

    ci(dockerfile): add clerk publishable key as arg and secret

--- apps/nextjs/src/app/api/webhooks/route.ts
+++ apps/nextjs/src/app/api/webhooks/route.ts
@@ -7,6 +7,8 @@ import { db } from "@planty/db/client";
 import { CreateUserSchema, User } from "@planty/db/schema";
 import { getSecretOrEnv } from "@planty/utils";
 
+
+
 export async function POST(req: Request) {
   const WEBHOOK_SECRET = await getSecretOrEnv("WEBHOOK_SECRET");
 
