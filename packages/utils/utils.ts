58df68102c23c410feae24530d2f11a3e9071abb
dobzacode <contact@corentinkittel.fr>
2024-05-29 21:24:26 +0200

    refactor(utils): remove the secret manager call with env as it is no longer needed

--- packages/utils/utils.ts
+++ packages/utils/utils.ts
@@ -1,7 +1,3 @@
-import {
-  GetSecretValueCommand,
-  SecretsManagerClient,
-} from "@aws-sdk/client-secrets-manager";
 import { createEnv } from "@t3-oss/env-nextjs";
 import { vercel } from "@t3-oss/env-nextjs/presets";
 import { ClassValue, clsx } from "clsx";
@@ -42,38 +38,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-async function getSecret(secretName: keyof typeof env): Promise<string> {
-  const client = new SecretsManagerClient({
-    region: "eu-central-1",
-  });
-
-  try {
-    const response = await client.send(
-      new GetSecretValueCommand({
-        SecretId: "prod/planty/PGSQL",
-        VersionStage: "AWSCURRENT",
-      }),
-    );
-
-    if (!response || !response.SecretString) {
-      throw new Error("Secret not found or SecretString is empty");
-    }
-
-    const parsed = JSON.parse(response.SecretString);
-
-    return parsed[secretName];
-  } catch (error) {
-    throw error;
-  }
-}
-
 export async function getSecretOrEnv(
   secretName: keyof typeof env,
 ): Promise<string> {
-  const value =
-    env.NODE_ENV !== "development"
-      ? env[secretName]
-      : await getSecret(secretName);
+  const value = env[secretName];
 
   if (!value) {
     throw new Error(
